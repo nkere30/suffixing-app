2b484a71d61db00dab2705a75eb84ff6d10593fc
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-10-27 02:15:43 +0400

    Suffixing: Commit N3

--- src/main/java/Suffixing.java
+++ src/main/java/Suffixing.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -9,39 +10,60 @@ public class Suffixing {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        try {
-            String pathToConfig = args[0];
-            InputStream inputStream = new FileInputStream(pathToConfig);
+        String pathToConfig = args[0];
+        try (InputStream inputStream = new FileInputStream(pathToConfig)){
             properties.load(inputStream);
-            String mode = properties.getProperty("mode");
-            String suffix = properties.getProperty("suffix");
-            String files = properties.getProperty("files");
-            if (!Objects.equals(mode, "copy")) {
-                logger.log(Level.SEVERE, "Mode is not recognized: " + mode);
-            }
-            if (Objects.equals(suffix, null) || suffix.isBlank()) {
-                logger.log(Level.SEVERE, "No suffix is configured");
-            }
-            if (Objects.equals(files, null) || files.isBlank()) {
-                logger.log(Level.WARNING, "No files are configured to be copied/moved");
-            }
-            String[] filesArray = files.split(":(?=[A-Za-z]:\\/)");
-            for (String file : filesArray) {
-                File filename = new File(file);
-                if (!filename.exists()) {
-                    logger.log(Level.SEVERE, "No such file ", filename);
-                } else {
-                    int index = file.lastIndexOf(".");
-                    String suffixedFile = file.substring(0, index) + "-" + file.substring(index);
-                    File path = new File(suffixedFile);
-                    logger.log(Level.INFO, suffixedFile + "-->" + path);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        String mode = properties.getProperty("mode");
+        String suffix = properties.getProperty("suffix");
+        String files = properties.getProperty("files");
+        checkCorrectFieldsForFile(mode, suffix, files);
     }
 
+    private static void checkCorrectFieldsForFile(String mode, String suffix, String files) {
+        if (Objects.equals(suffix, null) || suffix.isBlank()) {
+            logger.log(Level.SEVERE, "No suffix is configured");
+        } else if (Objects.equals(files, null) || files.isBlank()) {
+            logger.log(Level.WARNING, "No files are configured to be copied/moved");
+        } else if (!Objects.equals(mode.toUpperCase(), "COPY") && !Objects.equals(mode.toUpperCase(), "MOVE")) {
+            logger.log(Level.SEVERE, "Mode is not recognized: {0}", mode);
+        } else {
+            suffixFiles(files, suffix, mode);
+        }
+    }
+
+    private static void suffixFiles(String files, String suffix, String mode) {
+        String[] filesArray = files.split(":");
+        for (String file : filesArray) {
+            File filename = new File(file);
+            if (!filename.exists()) {
+                logger.log(Level.SEVERE, "No such file: {0}", file);
+                continue;
+            }
+            int index = file.lastIndexOf(".");
+            String suffixedFilePath = file.substring(0, index) + suffix + file.substring(index);
+            File path = new File(suffixedFilePath);
+            copyFileAndDisplayInfo(mode, file, filename, path, suffixedFilePath);
+        }
+    }
+
+    private static void copyFileAndDisplayInfo(String mode, String file, File filename, File path, String suffixedFilePath) {
+        try {
+            Files.copy(filename.toPath(), path.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (Objects.equals(mode.toUpperCase(), "MOVE")) {
+            try {
+                Files.delete(filename.toPath());
+                logger.log(Level.INFO, file + " => " + suffixedFilePath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if(Objects.equals(mode.toUpperCase(), "COPY")){
+            logger.log(Level.INFO, file + " -> " + suffixedFilePath);
+        }
+    }
 }
